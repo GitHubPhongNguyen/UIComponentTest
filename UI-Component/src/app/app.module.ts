edcc8329f2c9d722416e0cf92c8e33022a144c74
Phong Nguyen <nguyenvanphong5998@gmail.com>
2020-10-06 15:39:00 +0700

    Create card-trying component

--- UI-Component/src/app/app.module.ts
+++ UI-Component/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardTryingComponent } from './card-trying/card-trying.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardTryingComponent
   ],
   imports: [
     BrowserModule
---
+++ UI-Component/src/app/card-trying/card-trying.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-trying',
+  templateUrl: './card-trying.component.html',
+  styleUrls: ['./card-trying.component.scss']
+})
+export class CardTryingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
